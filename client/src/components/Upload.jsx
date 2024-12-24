82a74f40218479adf7f3ec6e9d3033eea2b28d77
Philip <membaphilip1@gmail.com>
2024-12-24 21:19:19 +0300

    able to add a pic

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import Axios from "axios";
+import {Image} from "cloudinary-react"
 
 function Upload() {
+  const [imageSelected, setImageSelected] = useState();
+  const [uploadedImageUrl,setUploadedImageUrl]=useState("")
+
+  const uploadImage = () => {
+    // console.log(files[0])
+    const formData = new FormData();
+    formData.append("file", imageSelected);
+    formData.append("upload_preset", "azoncode");
+
+    Axios.post(
+      "https://api.cloudinary.com/v1_1/dia2le5vz/image/upload",
+      formData
+    ).then((res) => {
+      console.log(res);
+      setUploadedImageUrl(res.data.secure_url)
+    });
+  };
+
   return (
     <div>
-        <div>
-            <h1>A button to upload images</h1>
-        </div>
-        <div>
-            <input type="file" />
-        </div>
+      <div>
+        <h1>A button to upload images</h1>
+      </div>
+      <div>
+        <input
+          type="file"
+          onChange={(event) => {
+            setImageSelected(event.target.files[0]);
+          }}
+        />
+        <button onClick={uploadImage}> Upload image</button>
+        <Image cloudName="dia2le5vz" publicId={uploadedImageUrl}/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Upload
+export default Upload;
